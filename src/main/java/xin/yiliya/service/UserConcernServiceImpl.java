6d889ba66ddf2e57b4bd636680272dbba89c50c3
aiurmaple <31840624+aiurmaple@users.noreply.github.com>
2017-11-24 23:51:17 +0800

    更新

--- src/main/java/xin/yiliya/service/UserConcernServiceImpl.java
+++ src/main/java/xin/yiliya/service/UserConcernServiceImpl.java
@@ -4,21 +4,31 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import xin.yiliya.dao.ConcernServiceMapper;
+import xin.yiliya.dao.ConcernStoreMapper;
 import xin.yiliya.dao.OfferServiceMapper;
 import xin.yiliya.dao.StoreMapper;
-import xin.yiliya.pojo.OfferServiceSimple;
-import xin.yiliya.pojo.StoreInfo;
+import xin.yiliya.pojo.*;
 
+import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class UserConcernServiceImpl implements UserConcernService{
 
-    @Autowired
-    OfferServiceMapper offerServiceMapper;
+    @Resource
+    private OfferServiceMapper offerServiceMapper;
 
-    @Autowired
-    StoreMapper storeMapper;
+    @Resource
+    private StoreMapper storeMapper;
+
+    @Resource
+    private ConcernServiceMapper concernServiceMapper;
+
+    @Resource
+    private ConcernStoreMapper concernStoreMapper;
+
 
     public PageInfo<OfferServiceSimple> userConcernServices(Integer userId, int currentPage, int pageSize) {
         PageHelper.startPage(currentPage,pageSize);
@@ -32,5 +42,67 @@ public class UserConcernServiceImpl implements UserConcernService{
         List<StoreInfo> list=storeMapper.getAllUserConcernStores(userId);
         PageInfo<StoreInfo> info=new PageInfo<StoreInfo>(list);
         return info;
+    }
+
+    public Boolean concernServiceJudgement(ConcernServiceKey key) {
+        try{
+            if(concernServiceMapper.concernServiceJudgement(key)!=0){
+                return true;
+            }else{
+                return false;
+            }
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public Boolean concernStoreJudgement(ConcernStoreKey key) {
+        try{
+            if(concernStoreMapper.concernStoreJudgement(key)!=0){
+                return true;
+            }else {
+                return false;
+            }
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public Boolean concernService(ConcernService concernService) {
+        try{
+            concernService.setTime(new Date());
+            concernServiceMapper.insertSelective(concernService);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public Boolean concernStore(ConcernStore concernStore) {
+        try{
+            concernStore.setTime(new Date());
+            concernStoreMapper.insertSelective(concernStore);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public Boolean unconcernService(ConcernServiceKey key) {
+        try{
+            concernServiceMapper.deleteByPrimaryKey(key);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public Boolean unconcernStore(ConcernStoreKey key) {
+        try{
+            concernStoreMapper.deleteByPrimaryKey(key);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
     }
 }
