e9d042b953db0092d6708a0cca9182db639f3a7e
Hitsuki9 <31876031+Hitsuki9@users.noreply.github.com>
2017-12-10 20:44:29 +0800

    Revert "小更新"
    This reverts commit 8178e0774023dedb2fdb311e6ce4f6bfba1dfe7a.

--- src/main/webapp/resources/js/residentHome.js
+++ src/main/webapp/resources/js/residentHome.js
@@ -228,12 +228,6 @@ jQuery(document).ready(function($) {
 	});
 
 	//选择图片
-    var cropper = $('#head-img-wrap img').cropper({
-        aspectRatio: 1 / 1,
-        crop: function(data) {
-            // Output the result data for cropping image.
-        }
-    });
 	$("#btn-upload").change(function(event) {
 		var $file = $(this);
 		var fileObj = $file[0];
@@ -245,8 +239,13 @@ jQuery(document).ready(function($) {
 			dataURL = windowURL.createObjectURL(fileObj.files[0]);
 			$img.attr('src',dataURL);
 			//cropper
-            $.fn.cropper;
-            cropper.cropper('reset').cropper('replace',dataURL);
+			$('#head-img-wrap img').cropper({
+  				aspectRatio: 1 / 1,
+  					crop: function(data) {
+    				// Output the result data for cropping image.
+  				}
+			});
+			$('#head-img-wrap img').cropper('replace', dataURL);
 		}
 		else{
 			dataURL = $file.val();
@@ -255,11 +254,6 @@ jQuery(document).ready(function($) {
 			imgObj.filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = dataURL;
 		}
 	});
-
-	$("#head .layui-btn").click(function(event) {
-		//获取canvas
-        var canvas = cropper.cropper('getCroppedCanvas');
-    });
 
 	//particles
 	particlesJS("particles-js", {
