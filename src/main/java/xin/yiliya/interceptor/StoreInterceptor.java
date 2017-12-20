6db02e08c6c9ce2ff8e1aadfda545a9919679594
aiurmaple <31840624+aiurmaple@users.noreply.github.com>
2017-12-20 19:50:17 +0800

    更新

--- src/main/java/xin/yiliya/interceptor/StoreInterceptor.java
+++ src/main/java/xin/yiliya/interceptor/StoreInterceptor.java
@@ -3,12 +3,26 @@ package xin.yiliya.interceptor;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import xin.yiliya.pojo.Store;
+import xin.yiliya.pojo.StoreInfo;
+import xin.yiliya.service.EvaluateStoreService;
+import xin.yiliya.service.StoreNewsService;
+import xin.yiliya.service.StoreService;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class StoreInterceptor implements HandlerInterceptor {
+
+    @Resource
+    private StoreService storeService;
+
+    @Resource
+    private EvaluateStoreService evaluateStoreService;
+
+    @Resource
+    private StoreNewsService storeNewsService;
 
     public boolean preHandle(HttpServletRequest httpServletRequest,
                              HttpServletResponse httpServletResponse, Object o) throws Exception{
@@ -27,7 +41,13 @@ public class StoreInterceptor implements HandlerInterceptor {
                            HttpServletResponse httpServletResponse,
                            Object o,
                            ModelAndView modelAndView) throws Exception {
-
+        HttpSession session = httpServletRequest.getSession();
+        Store store= (Store) session.getAttribute("storeBean");
+        Integer storeId = store.getStoreid();
+        StoreInfo storeInfo = storeService.getInfoByStoreId(storeId);
+        modelAndView.addObject("storeInfo",storeInfo);
+        modelAndView.addObject("grade",evaluateStoreService.getGradeByStoreId(storeId));
+        modelAndView.addObject("unReadNewsNum",storeNewsService.getUnreadNumByStoreId(storeId));
     }
 
     public void afterCompletion(HttpServletRequest httpServletRequest,
